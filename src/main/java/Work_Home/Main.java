9c3c7fe8c23b499dfcd3874daad4a84e38bb98d2
Ruslan-Gusmanov <78360345+Ruslan-Gusmanov@users.noreply.github.com>
2024-12-01 16:53:32 +0300

    Update Main.java

--- src/main/java/Work_Home/Main.java
+++ src/main/java/Work_Home/Main.java
@@ -2,6 +2,6 @@ package Work_Home;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, qu-guru!");
+        System.out.println("Hello, qu-guru!!!!");
     }
 }
